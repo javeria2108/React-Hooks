7f94dea854dfc4321c2aac97b95453fdf61e85bf
javeria2108 <javeriazh@gmail.com>
2023-04-27 21:28:42 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from "react";
+import { useEffect } from "react";
+const SampleTask=()=>{
+  const[counter,setCounter]=useState(0);
+  const HandleClick=()=>setCounter(counter+1);
+  useEffect(()=>{
+    console.log('inside UseEffect');
+  },[])
+  return(
+    <>
+    
+      <button style={{marginTop: '50px'}} onClick={HandleClick}>Click Me</button>;
+      <div>{counter}</div>;
+    </>
+  )
 }
-
-export default App;
+export default SampleTask
